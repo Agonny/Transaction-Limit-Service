ddf410ed31488edfd319ccde3b31a800b13edc5c
Agonny <80535413+Agonny@users.noreply.github.com>
2025-04-07 03:01:19 +0300

    improve mappers

--- src/main/java/com/example/transaction_limit_service/mapper/LimitMapper.java
+++ src/main/java/com/example/transaction_limit_service/mapper/LimitMapper.java
@@ -1,7 +1,15 @@
 package com.example.transaction_limit_service.mapper;
 
+import com.example.transaction_limit_service.dto.LimitCreateDto;
+import com.example.transaction_limit_service.entity.Limit;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface LimitMapper {
+
+    LimitMapper INSTANCE = Mappers.getMapper(LimitMapper.class);
+
+    Limit toEntity(LimitCreateDto dto);
+
 }
--- src/main/java/com/example/transaction_limit_service/mapper/TransactionMapper.java
+++ src/main/java/com/example/transaction_limit_service/mapper/TransactionMapper.java
@@ -1,12 +1,22 @@
 package com.example.transaction_limit_service.mapper;
 
+import com.example.transaction_limit_service.dto.TransactionCreateDto;
 import com.example.transaction_limit_service.dto.TransactionDto;
 import com.example.transaction_limit_service.entity.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface TransactionMapper {
 
+    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
+
     TransactionDto toDto(Transaction entity);
+
+    Transaction toEntity(TransactionCreateDto entity);
+
+    List<TransactionDto> toListDto(List<Transaction> listOfEntities);
 
 }
